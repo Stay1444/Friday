5fa394317e6b4e8cc4b69174cc2a4aa2950e3c93
Stay1444 <quiquelluc@gmail.com>
2022-06-01 03:10:53 +0200

    Int -> BigInt

--- Modules/Friday.Modules.Moderation/Resources/required.sql
+++ Modules/Friday.Modules.Moderation/Resources/required.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `mod_user_bans` (
-    `user_id` int(64) NOT NULL,
-    `guild_id` int(64) NOT NULL,
-    `banned_by` int(64) NOT NULL,
+    `user_id` bigint(64) NOT NULL,
+    `guild_id` bigint(64) NOT NULL,
+    `banned_by` bigint(64) NOT NULL,
     `reason` TEXT NOT NULL,
     `banned_at` DATETIME NOT NULL,
     `expires_at` DATETIME NOT NULL
