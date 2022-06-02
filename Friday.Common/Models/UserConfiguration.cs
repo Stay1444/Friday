9e9f68eb1da8af0b428807ca8901526921bd1a5b
Stay1444 <quiquelluc@gmail.com>
2022-06-02 20:53:09 +0200

    Change property init -> set

--- Friday.Common/Models/UserConfiguration.cs
+++ Friday.Common/Models/UserConfiguration.cs
@@ -5,8 +5,8 @@ namespace Friday.Common.Models;
 public record UserConfiguration
 {
     [ColumnName("prefix_override")]
-    public string? PrefixOverride { get; init; }
+    public string? PrefixOverride { get; set; }
     
     [ColumnName("language_override")]
-    public string? LanguageOverride { get; init; }
+    public string? LanguageOverride { get; set; }
 }
