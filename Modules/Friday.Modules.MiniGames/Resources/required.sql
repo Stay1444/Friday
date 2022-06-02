0c2e0ea953194e55ceb713096bcd72c946f8c135
Stay1444 <quiquelluc@gmail.com>
2022-06-02 02:47:51 +0200

    Fixed bug

--- Modules/Friday.Modules.MiniGames/Resources/required.sql
+++ Modules/Friday.Modules.MiniGames/Resources/required.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `mgs_2048_leaderboard` (
     `id` bigint(64) UNIQUE NOT NULL,
-    `max_score` bigint(64) UNIQUE NOT NULL,
-    `total_score` bigint(64) UNIQUE NOT NULL,
-    `played` bigint(64) UNIQUE NOT NULL,
-    `playtime_seconds` bigint(64) UNIQUE NOT NULL,
+    `max_score` bigint(64) NOT NULL,
+    `total_score` bigint(64) NOT NULL,
+    `played` bigint(64) NOT NULL,
+    `playtime_seconds` bigint(64) NOT NULL,
     `recorded_username` TEXT NOT NULL,
     PRIMARY KEY (`id`)
 )
