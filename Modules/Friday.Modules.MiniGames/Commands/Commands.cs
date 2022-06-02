24fa732674e5cb13e6de01a6fb04143da631a533
Stay1444 <quiquelluc@gmail.com>
2022-06-02 06:08:48 +0200

    Added requirements

--- Modules/Friday.Modules.MiniGames/Commands/Commands.cs
+++ Modules/Friday.Modules.MiniGames/Commands/Commands.cs
@@ -1,13 +1,18 @@
 using Friday.Common.Entities;
+using Friday.Common.Services;
+using Friday.Modules.MiniGames.Services;
 
 namespace Friday.Modules.MiniGames.Commands;
 
 public partial class Commands : FridayCommandModule
 {
     private MiniGamesModule _module;
-
-    public Commands(MiniGamesModule module)
+    private LanguageProvider _languageProvider;
+    private HangmanWordList _hangmanWordList;
+    public Commands(MiniGamesModule module, LanguageProvider languageProvider)
     {
         _module = module;
+        _languageProvider = languageProvider;
+        _hangmanWordList = new HangmanWordList();
     }
 }
