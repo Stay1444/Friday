0355d1ce95f46858b27c51914afc36efb229915a
Stay1444 <quiquelluc@gmail.com>
2022-06-02 20:51:40 +0200

    Added references to UserConfigurationProvider and GuildConfigurationProvider

--- Modules/Friday.Modules.Misc/Commands/Commands.cs
+++ Modules/Friday.Modules.Misc/Commands/Commands.cs
@@ -10,11 +10,15 @@ public partial class Commands : FridayCommandModule
     private FridayConfiguration _fridayConfiguration;
     private FridayVerifiedServerService _fridayVerifiedServer;
     private DatabaseProvider _databaseProvider;
-    public Commands(FridayModeratorService moderatorService, FridayConfiguration fridayConfiguration, FridayVerifiedServerService fridayVerifiedServer, DatabaseProvider databaseProvider)
+    private UserConfigurationProvider _userConfigurationProvider;
+    private GuildConfigurationProvider _guildConfigurationProvider;
+    public Commands(FridayModeratorService moderatorService, FridayConfiguration fridayConfiguration, FridayVerifiedServerService fridayVerifiedServer, DatabaseProvider databaseProvider, UserConfigurationProvider userConfigurationProvider, GuildConfigurationProvider guildConfigurationProvider)
     {
         _moderatorService = moderatorService;
         _fridayConfiguration = fridayConfiguration;
         _fridayVerifiedServer = fridayVerifiedServer;
         _databaseProvider = databaseProvider;
+        _userConfigurationProvider = userConfigurationProvider;
+        _guildConfigurationProvider = guildConfigurationProvider;
     }
 }
